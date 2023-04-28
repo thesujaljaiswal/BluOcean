288b35554f04fe636fc43ec947615c2fd9cbd2a0
anjaanid <sujal676865@ascs.sies.edu.in>
2023-04-28 17:58:47 +0530

    added contact us

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import './App.css';
-import Contactus from './Components/Contactus';
-import Content from './Components/Content';
-import Navbar from './Components/Navbar';
-import Footer from './Components/footer';
+import "./App.css";
+import Contactus from "./Components/Contactus";
+import Content from "./Components/Content";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/footer";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Content maintitle="Contact Us"/>
-    <Contactus/>
-    <Footer/>
+      {/* <Router> */}
+        <Navbar />
+        {/* <Switch>
+          <Route path="/contactus"> */}
+            <Content maintitle="Contact Us" />
+            <Contactus />
+          {/* </Route>
+        </Switch> */}
+        <Footer />
+      {/* </Router> */}
     </>
   );
 }
--- src/Components/Contactus.js
+++ src/Components/Contactus.js
@@ -4,15 +4,59 @@ export default function Contactus() {
   return (
     <>
       <section className="text-gray-800 body-font relative">
-        <div className="container px-5 py-24 mx-auto">
+        <div class="flex flex-wrap -m-2 w-full pt-10 px-5">
+          <div class="p-2 lg:w-1/3 md:w-1/2">
+            <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg bg-indigo-300">
+              <img
+                alt="team"
+                class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                src={require("./contact.png")}
+              />
+              <div class="flex-grow">
+                <h2 class="text-gray-900 title-font font-medium">
+                  Email Address
+                </h2>
+                <p class="text-gray-800">bluocean001@gmail.com</p>
+              </div>
+            </div>
+          </div>
+          <div class="p-2 lg:w-1/3 md:w-1/2">
+            <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg bg-indigo-300">
+              <img
+                alt="team"
+                class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                src={require("./Location.png")}
+              />
+              <div class="flex-grow">
+                <h2 class="text-gray-900 title-font font-medium">
+                  Our Address
+                </h2>
+                <p class="text-gray-800">UI Designer</p>
+              </div>
+            </div>
+          </div>
+          <div class="p-2 lg:w-1/3 md:w-1/2">
+            <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg bg-indigo-300">
+              <img
+                alt="team"
+                class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                src={require("./timing.png")}
+              />
+              <div class="flex-grow">
+                <h2 class="text-gray-900 title-font font-medium">Hours</h2>
+                <p class="text-gray-800">Mon – Sat: 9am – 6pm<br/>Sun: closed</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container px-5 py-10 mx-auto">
           <div className="flex flex-col text-center w-full mb-12">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
               Contact Us
             </h1>
-            <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-800">
-              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
-              gentrify.
-            </p>
+            <h1 className="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-800">
+              EFFECTIVE MANAGEMENT CONSULTANTS.
+            </h1>
           </div>
           <div className="lg:w-1/2 md:w-2/3 mx-auto">
             <div className="flex flex-wrap -m-2">
@@ -146,33 +190,23 @@ export default function Contactus() {
               </div>
               <div className="p-2 w-full">
                 <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                  Button
+                  Submit
                 </button>
               </div>
             </div>
           </div>
         </div>
-        <section class="text-gray-600 body-font relative">
-  <div class="absolute inset-0 bg-gray-300">
-    <iframe width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="map" scrolling="no" src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"></iframe>
-  </div>
-  <div class="container px-5 py-24 mx-auto flex">
-    <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-      <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
-      <p class="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
-      <div class="relative mb-4">
-        <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-        <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
-      </div>
-      <div class="relative mb-4">
-        <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-        <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-      </div>
-      <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-      <p class="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
-    </div>
-  </div>
-</section>
+        <div class="bg-gray-300">
+          <iframe
+            title="Adress"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3765.3656760211425!2d72.855805!3d19.3099322!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b134742ae5d9%3A0xe38ea07d339ee004!2sSAINATH%20INDUSTRIAL%20M.K.B!5e0!3m2!1sen!2sin!4v1682676473692!5m2!1sen!2sin"
+            width="100%"
+            height="450"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
       </section>
     </>
   );
