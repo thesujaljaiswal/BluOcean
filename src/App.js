e36f43348f5f07eb6fed914137e77e7874e10f81
anjaanid <sujal676865@ascs.sies.edu.in>
2023-04-28 11:30:24 +0530

    Added Central Div Which Shows All the Content

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Content from './Components/Content';
 import Navbar from './Components/Navbar';
 import Footer from './Components/footer';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
     <Navbar/>
+    <Content maintitle="Apply Now"/>
     <Footer/>
     </>
   );
---
+++ src/Components/Content.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Content.css';
+
+export default function Content(props) {
+  return (
+    <>
+      <div className="page-details-container">
+        <div>
+            <p className="font-sans text-5xl font-bold pl-10">{props.maintitle}</p>
+        </div>
+        <div>
+            <p className="font-sans text-2xl pl-10"><a href="/" className="text-gray-50 hover:text-blue-600">Blu Ocean</a> &gt; {props.maintitle}</p>
+        </div>
+      </div>
+    </>
+  );
+}
