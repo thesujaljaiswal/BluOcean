de725e141d26383d5e472afac188feeb162fdda7
anjaanid <sujal676865@ascs.sies.edu.in>
2023-04-30 19:56:01 +0530

    changes in home pages section of ritesh

--- src/Components/Points.js
+++ src/Components/Points.js
@@ -3,26 +3,27 @@ import React from 'react'
 export default function Points(props) {
   return (
     <>
-      <p> {props.content1}</p><br/>
-      <p> {props.content2}</p><br/>
-      <p> {props.content3}</p><br/>
-      <p> {props.content4}</p><br/>
-      <p> {props.content5}</p><br/>
-      <p> {props.content6}</p><br/>
-      <p> {props.content7}</p><br/>
-      <p> {props.content8}</p><br/>
-      <p> {props.content9}</p><br/>
-      <p> {props.content10}</p><br/>
-      <p> {props.content11}</p><br/>
-      <p> {props.content12}</p><br/>
-      <p> {props.content13}</p><br/>
-      <p> {props.content14}</p><br/>
-      <p> {props.content15}</p><br/>
-      <p> {props.content16}</p><br/>
-      <p> {props.content17}</p><br/>
-      <p> {props.content18}</p><br/>
-      <p> {props.content19}</p><br/>
+      <p> {props.content1}</p>
+      <p> {props.content2}</p>
+      <p> {props.content3}</p>
+      <p> {props.content4}</p>
+      <p> {props.content5}</p>
+      <p> {props.content6}</p>
+      <p> {props.content7}</p>
+      <p> {props.content8}</p>
+      <p> {props.content9}</p>
+      <p> {props.content10}</p>
+      <p> {props.content11}</p>
+      <p> {props.content12}</p>
+      <p> {props.content13}</p>
+      <p> {props.content14}</p>
+      <p> {props.content15}</p>
+      <p> {props.content16}</p>
+      <p> {props.content17}</p>
+      <p> {props.content18}</p>
+      <p> {props.content19}</p>
       <p> {props.content20}</p>
+      <button className="text-orange-500 border-2 border-orange-500  py-4 px-8 hover:bg-orange-500 hover:text-gray-100 rounded text-lg w-40">Apply Now</button>
     </>
   )
 }
