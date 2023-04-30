0d2427b0857227e745e1e177324d0f06d9352c3c
chabarkar7 <riteshchabarkar647@gmail.com>
2023-04-30 19:06:46 +0530

    added homepage files of ritesh

---
+++ src/Components/Points.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function Points(props) {
+  return (
+    <>
+      <p> {props.content1}</p><br/>
+      <p> {props.content2}</p><br/>
+      <p> {props.content3}</p><br/>
+      <p> {props.content4}</p><br/>
+      <p> {props.content5}</p><br/>
+      <p> {props.content6}</p><br/>
+      <p> {props.content7}</p><br/>
+      <p> {props.content8}</p><br/>
+      <p> {props.content9}</p><br/>
+      <p> {props.content10}</p><br/>
+      <p> {props.content11}</p><br/>
+      <p> {props.content12}</p><br/>
+      <p> {props.content13}</p><br/>
+      <p> {props.content14}</p><br/>
+      <p> {props.content15}</p><br/>
+      <p> {props.content16}</p><br/>
+      <p> {props.content17}</p><br/>
+      <p> {props.content18}</p><br/>
+      <p> {props.content19}</p><br/>
+      <p> {props.content20}</p>
+    </>
+  )
+}
